02f671ada3c6aa2f5fe90f1e6db286108cc39207
Dog <104234930+dgxo@users.noreply.github.com>
2026-02-06 21:23:58 +0000

    Move endpoints under /api and normalize player data
    These changes are mostly backwards compatible with pre-h7, but
    third-party tools using /ws will have to change to /api/ws

--- server.ts
+++ server.ts
@@ -1,12 +1,24 @@
-import { Hono } from "hono";
-import { serveStatic } from "hono/bun";
-import { upgradeWebSocket, websocket } from "hono/bun";
+import { Context, Hono } from "hono";
+import { serveStatic, upgradeWebSocket, websocket } from "hono/bun";
 import { WSContext } from "hono/ws";
-import z from "zod";
+import z from "zod/v4";
 
-const trainSchema = z.array(z.string()).length(4);
+const playersCache: Map<string, number[]> = new Map();
+const serverTimeouts: Map<string, NodeJS.Timeout> = new Map();
+
+const trainSchema = z.xor([
+	z.array(z.string()).length(4),
+	z.strictObject({
+		destination: z.string(),
+		trainClass: z.string(),
+		headcode: z.string(),
+		trainType: z.string(),
+	}),
+]);
+
 const playerSchema = z.strictObject({
 	username: z.string(),
+	userId: z.number().optional(),
 	trainData: trainSchema.optional(),
 	position: z.strictObject({
 		x: z.int(),
@@ -16,26 +28,29 @@ const playerSchema = z.strictObject({
 const requestSchema = z.strictObject({
 	jobId: z.string(),
 	players: z.array(playerSchema),
-	// deprecated
-	token: z.string().optional(),
 });
 
 const app = new Hono();
 const PORT = process.env.PORT || 3000;
-const ROBLOX_TOKEN = process.env.ROBLOX_OTHER_KEY;
+const STALE_SERVER_TIMEOUT = 30_000;
 
+const ROBLOX_TOKEN = process.env.ROBLOX_OTHER_KEY;
 if (!ROBLOX_TOKEN) throw new Error(`Token environment variable is missing`);
+
+const GET_PLAYERS_API_KEY = process.env.GET_PLAYERS_API_KEY;
+if (!GET_PLAYERS_API_KEY)
+	throw new Error(`GET players API key environment variable is missing`);
 
 app.use("*", serveStatic({ root: "./public" }));
 
 let webSockets: WSContext<any>[] = [];
 
-app.get("/status", (context) => {
+app.get("/api/status", (context) => {
 	return context.text("200 OK");
 });
 
 app.get(
-	"/ws",
+	"/api/ws",
 	upgradeWebSocket((context) => {
 		return {
 			onOpen: (_event, webSocket) => {
@@ -51,29 +66,80 @@ app.get(
 	}),
 );
 
-app.post("/positions", async (context) => {
+app.use("/api/*", async (context, next) => {
+	const authorizationHeader = context.req.header("Authorization");
+	if (authorizationHeader !== `Bearer ${ROBLOX_TOKEN}`) {
+		return context.text("Invalid token", 401);
+	}
+	await next();
+});
+
+app.get("/api/servers/:jobId/players", async (context) => {
+	const jobId = context.req.param("jobId");
+	return context.json(playersCache.get(jobId));
+});
+
+async function positionsApi(context: Context) {
 	const result = requestSchema.safeParse(await context.req.json());
 
 	if (!result.success) {
-		return context.json({ success: false, errors: result.error.issues }, 400);
+		return context.json(
+			{ success: false, errors: z.treeifyError(result.error) },
+			400,
+		);
 	}
-
-	const { token: bodyToken, ...dataToSend } = result.data;
-	// once all Dovedale servers migrate to version using headers, body token support will be removed
 	const authorizationHeader = context.req.header("Authorization");
-	if (
-		authorizationHeader !== `Bearer ${ROBLOX_TOKEN}` &&
-		bodyToken !== ROBLOX_TOKEN
-	) {
+	if (authorizationHeader !== `Bearer ${ROBLOX_TOKEN}`) {
 		return context.text("Invalid token", 401);
 	}
 
+	result.data.players = result.data.players.map((player) => ({
+		username: player.username,
+		userId: player.userId,
+		position: player.position,
+		// will be completely replaced after servers have been migrated to h7
+		trainData: Array.isArray(player.trainData)
+			? {
+					destination: player.trainData[0],
+					trainClass: player.trainData[1],
+					headcode: player.trainData[2],
+					trainType: player.trainData[3],
+				}
+			: player.trainData,
+	}));
+
+	if (serverTimeouts.has(result.data.jobId)) {
+		console.log("Clearing current timeout");
+		serverTimeouts.delete(result.data.jobId);
+	}
+
+	serverTimeouts.set(
+		result.data.jobId,
+		setTimeout(() => {
+			console.log("Clearing, timeout have passed");
+			playersCache.delete(result.data.jobId);
+			serverTimeouts.delete(result.data.jobId);
+		}, STALE_SERVER_TIMEOUT),
+	);
+
+	if (result.data.players[0]?.userId) {
+		playersCache.set(
+			result.data.jobId,
+			result.data.players
+				.filter((player) => player.userId !== undefined)
+				.map((player) => player.userId as number),
+		);
+	}
+
 	webSockets.forEach((webSocket) => {
-		webSocket.send(JSON.stringify(dataToSend));
+		webSocket.send(JSON.stringify(result.data));
 	});
 
 	return context.json({ success: true });
-});
+}
+
+app.post("/api/positions", positionsApi);
+app.post("/positions", positionsApi);
 
 export default {
 	fetch: app.fetch,
